118a130daf66d0e95d6900a085c80252d1545276
Atharv1056 <atharv.ambre24@sanjivani.edu.in>
2024-08-16 19:34:25 +0530

    Add files via upload

---
+++ Lab 2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int a;
+  cout<<"Enter the number"<<endl;
+  cin>>a;
+  /*logic*/
+  if(a%2==0)
+  {
+    cout<<"The given number is Even"<<endl;
+  }
+  else
+  {
+    cout<<"The given number is Odd"<<endl;
+  }
+  return 0;
+}
